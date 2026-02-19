517e37a9cdc52b4f9c68e311f81a252cd866d469
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2026-02-20 00:45:59 +0530

    Updated the dawn script for entries db
    - Added dynamic directory creation based on the current Month and Day.
    - Implemented daily `entries.json` to store and update grouped learnings for multiple users.
    - Created dynamic Markdown file generation (`[username]_learning.md`) using string subexpressions and the `-join` operator for bulleted lists.
    - Added a master `userdb.json` registry to track all unique users who run the script.
    - Handled array coercion and raw string reading to resolve JSON type mismatch errors.

--- dawn.ps1
+++ dawn.ps1
@@ -7,27 +7,71 @@ $day = Get-Date -Format 'dd'
 $month = Get-Date -Format 'MMMM'
 $year = Get-Date -Format 'yyyy'
 
-$readme = "# Krishna Learning
----
-### $day, $month $year
 
-## What did I learn today ?
----
-
-$entry
-"
 Write-Output $day $month
 
-if (!{Test-Path ./$month}){
+if (-not(Test-Path ./$month)){
 
     New-Item -Path "./" -Name $month -ItemType Directory
 }
-if (!{Test-Path ./$month/$day}){
+if (-not(Test-Path ./$month/$day)){
     New-Item -Path "./$month" -Name $day -ItemType Directory
 
 }
 
-New-Item -Path "./$month/$day" -Name "${name}_learning.md" -Value $readme -Force
+
+
+
+# ENTRIES DATABASE FOR EACH DAY
+
+# Check if entry db is present
+$check_entries_db = Test-Path "./$month/$day/entries.json"
+
+
+if (!$check_entries_db) {
+    New-Item -Path ./$month/$day/ -Name 'entries.json' -Value "[]"
+}
+
+$entries = Get-Content "./$month/$day/entries.json" -Raw
+
+[array]$edb = ConvertFrom-Json $entries
+
+$user = $edb | Where-Object { $_.User -eq $name }
+
+
+if (-not $user) {
+    $new_record = [PSCustomObject]@{
+        User     = $name.ToLower()
+        Learning = @($entry)
+    }
+    $edb += $new_record
+}
+else {
+    $user.Learning += $entry
+}
+
+$edb_json = ConvertTo-Json $edb
+
+Set-Content "./$month/$day/entries.json" -Value $edb_json
+
+
+Write-Output "Entries record saved to Json"
+
+$user = $edb | Where-Object { $_.User -eq $name }
+
+$all_learning = $user.Learning -join "`n`t- "
+
+$readme = "# $($name.ToUpper()) Learning
+
+### $day, $month $year
+
+## What did I learn today ?
+
+`t- $all_learning
+"
+
+New-Item -Path "./$month/$day" -Name "$($name.ToLower())_learning.md" -Value $readme -Force
+
 
 # USER DATABASE CHECK
 
